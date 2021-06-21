1514997c3081b9b337f8245f510dfc18b85999ea
that-nanjala <nanjalaliz7@gmail.com>
2021-06-21 20:48:18 +0300

    drop duplicate rows

--- clean_tweets_dataframe.py
+++ clean_tweets_dataframe.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 class Clean_Tweets:
     """
     The PEP8 Standard AMAZING!!!
@@ -20,8 +22,8 @@ class Clean_Tweets:
         """
         drop duplicate rows
         """
-        
-        ---
+        df = df.drop_duplicates()
+    
         
         return df
     def convert_to_datetime(self, df:pd.DataFrame)->pd.DataFrame:
