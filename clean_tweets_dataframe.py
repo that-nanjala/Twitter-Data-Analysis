ac88136a78baa29b9c7a993463ce1ce0a8ff06b6
that-nanjala <nanjalaliz7@gmail.com>
2021-06-21 21:36:18 +0300

    convert columns to numbers

--- clean_tweets_dataframe.py
+++ clean_tweets_dataframe.py
@@ -31,7 +31,7 @@ class Clean_Tweets:
         convert column to datetime
         """
         df["created_at"] = pd.to_datetime(df["created_at"])
-        
+
         df = df[df['created_at'] >= '2020-12-31' ]
         
         return df
@@ -41,10 +41,13 @@ class Clean_Tweets:
         convert columns like polarity, subjectivity, retweet_count
         favorite_count etc to numbers
         """
-        df['polarity'] = pd.----
-        
-        ----
-        ----
+        df['polarity'] = pd.to_numeric(df.polarity)
+
+        df['subjectivity'] = pd.to_numeric(df.subjectivity)
+
+        df['retweet_count'] = pd.to_numeric(df.retweet_count)
+
+        df['favorite_count'] = pd.to_numeric(df.favorite_count)
         
         return df
     
